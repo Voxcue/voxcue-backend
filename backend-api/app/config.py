3a2a6f8f1b1a8ed6bb04fe675cbd6135a57be39e
Abhinav Unnikrishnan <abhinavdev@Abhinavs-MacBook-Pro.local>
2025-01-31 20:38:45 +0530

    celery set

--- backend-api/app/config.py
+++ backend-api/app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SUPABASE_URL = os.getenv('SUPABASE_URL')
--- backend-api/test_db.py
+++ backend-api/test_db.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
