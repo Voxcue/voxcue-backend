ec9ec81f6c0b4e33342e7604d502c7eae029fd7b
Abhinav Unnikrishnan <abhinavdev@Abhinavs-MacBook-Pro.local>
2025-02-04 21:27:00 +0530

    celery-beat setted

--- backend-api/app/__init__.py
+++ backend-api/app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from app.extensions import db, migrate
 from app.auth.routes import auth_bp
-from celery import Celery
+from celery import Celery,shared_task
 
 
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        broker=app.config.get("CELERY_BROKER_URL"),
-        backend=app.config.get("CELERY_RESULT_BACKEND"),
+        broker=app.config["broker_url"],
+        backend=app.config["result_backend"],
     )
     celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
     return celery
 
 
@@ -18,8 +25,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
-    app.config["CELERY_BROKER_URL"] = "redis://redis:6379/0"
-    app.config["CELERY_RESULT_BACKEND"] = "redis://redis:6379/0"
+    # Use new-style configuration keys
+    app.config["broker_url"] = "redis://redis:6379/0"
+    app.config["result_backend"] = "redis://redis:6379/0"
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -27,4 +35,23 @@ def create_app():
     app.register_blueprint(auth_bp)
 
     celery = make_celery(app)
+
+    celery.conf.beat_schedule = {
+        "run-every-30-seconds": {
+            "task": "app.auth.tasks.add_together",
+            "schedule": 30.0,
+        },
+        "run-every-5-seconds": {
+            "task": "app.auth.tasks.add_together",
+            "schedule": 5.0,
+        },
+    }
+
+
+    # example beat Process
+    @shared_task(name="app.auth.tasks.add_together")
+    def add_together(a=1, b=2):
+        """Simple arithmetic task for testing"""
+        return a + b
+
     return app, celery
